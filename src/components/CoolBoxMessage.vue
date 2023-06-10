2d9c90959094eddda04870b348347d3fac200e7f
T0RNATO <nlittle315@gmail.com>
2023-06-10 22:45:00 +1000

    Add extension settings and feedback message on homepage

---
+++ src/components/CoolBoxMessage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="pl-2 text-sm">
+        <h1 class="mt-2">CoolBox</h1>
+        <span id="cb-settings">
+            Edit extension settings (like enabling dark mode!) by clicking the CoolBox extension icon in the extensions menu (top right).<br>
+            View a full list of features at <a href="https://coolbox.lol">coolbox.lol</a>.<br>
+            Got questions, comments, suggestions? Send us feedback!
+        </span>
+        <button id="cb-feedback" class="block my-4">Submit Feedback</button>
+        <span class="italic text-gray-400">
+            CoolBox is not endorsed by or affiliated with Donvale Christian College or Schoolbox Pty Ltd.<br>
+            This section can be removed from the extension settings.
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CoolBoxMessage"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/entries/contentScript/primary/Homepage.vue
+++ src/entries/contentScript/primary/Homepage.vue
@@ -1,31 +1,39 @@
 <template>
-  <div>
-    <div class="grid grid-cols-[65%,35%]">
-      <!-- Left Column -->
-      <div class="px-4">
-        <GreetingText />
-        <hr>
-        <Tiles />
-      </div>
-      <!-- Right Column -->
-      <div class="px-4">
-        <UpcomingDueWork />
-      </div>
+    <div>
+        <div class="grid grid-cols-[65%,35%]">
+            <!-- Left Column -->
+            <div class="px-4">
+                <GreetingText />
+                <hr>
+                <Tiles />
+                <CoolBoxMessage v-if="show_feedback"/>
+            </div>
+            <!-- Right Column -->
+            <div class="px-4">
+                <UpcomingDueWork />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import GreetingText from "~/components/GreetingText.vue";
 import UpcomingDueWork from "~/components/UpcomingDueWork.vue";
 import Tiles from "~/components/Tiles.vue";
+import CoolBoxMessage from "~/components/CoolBoxMessage.vue";
+import {ref} from "vue";
+import browser from "webextension-polyfill";
 
-let count = ref(0);
+const show_feedback = ref(true)
 
-function increaseCount() {
-  count.value += 1;
-}
+browser.storage.sync.get("feedback").then((result) => {
+    show_feedback.value = !result.feedback
+});
+browser.storage.sync.onChanged.addListener((changes) => {
+    if (changes.feedback) {
+        show_feedback.value = !changes.feedback.newValue;
+    }
+})
 </script>
 
 <style>
