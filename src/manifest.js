bf7cec7a18d2b73ab8fdda1b6af98d02f95d3ba2
T0RNATO <nlittle315@gmail.com>
2023-06-10 15:21:16 +1000

    Update icon

--- src/manifest.js
+++ src/manifest.js
@@ -8,16 +8,7 @@ const sharedManifest = {
     },
   ],
   icons: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
-    48: "icons/48.png",
-    64: "icons/64.png",
-    96: "icons/96.png",
-    128: "icons/128.png",
-    256: "icons/256.png",
-    512: "icons/512.png",
+    64: "icons/icon.png"
   },
   options_ui: {
     page: "src/entries/options/index.html",
@@ -28,10 +19,7 @@ const sharedManifest = {
 
 const browserAction = {
   default_icon: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
+    64: "icons/icon.png"
   },
   default_popup: "src/entries/popup/index.html",
 };
