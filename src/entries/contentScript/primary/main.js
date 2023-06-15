6d3b0729f996799d3c153dd8221dc7013d92e57a
T0RNATO <nlittle315@gmail.com>
2023-06-15 10:51:06 +1000

    Fixed the extension removing all content from pages other than the homepage

--- src/entries/contentScript/primary/main.js
+++ src/entries/contentScript/primary/main.js
@@ -3,6 +3,8 @@ import renderContent from "../renderContent";
 import App from "./Homepage.vue";
 import "tailwindcss/tailwind.css";
 
-renderContent(import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS, (appRoot) => {
-  createApp(App).mount(appRoot);
-});
+if (location.href === "https://schoolbox.donvale.vic.edu.au/") {
+  renderContent(import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS, (appRoot) => {
+    createApp(App).mount(appRoot);
+  });
+}
