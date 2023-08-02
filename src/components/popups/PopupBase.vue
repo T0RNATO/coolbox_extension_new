9d6224a87cccbdc9c8deb0beea026b244d0be2b1
T0RNATO <nlittle315@gmail.com>
2023-08-02 19:24:37 +1000

    Fixed border around popup

--- src/components/popups/PopupBase.vue
+++ src/components/popups/PopupBase.vue
@@ -1,5 +1,5 @@
 <template>
-    <dialog class="dui-modal">
+    <dialog class="dui-modal border-transparent">
         <div class="dui-modal-box p-0 rounded-sm shadow-2xl bg-primary" v-bind="$attrs">
             <h1 class="bg-[#325985] w-full !text-white p-4 mb-0">{{title}}</h1>
             <div class="p-4 pb-2 text-themeText">
@@ -9,7 +9,7 @@
         <!--Shadow root to prevent button styling-->
         <shadow-root :adopted-style-sheets="defaultSheets" class="dui-modal-backdrop">
             <form method="dialog" class="dui-modal-backdrop">
-                <button class="bg-black bg-transparent/20">close</button>
+                <button class="bg-black bg-transparent/20 border-transparent">close</button>
             </form>
         </shadow-root>
     </dialog>
