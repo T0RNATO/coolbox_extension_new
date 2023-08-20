c2ffeb506f6dfee0d24e316e08e7f2793d579172
T0RNATO <nlittle315@gmail.com>
2023-08-20 17:21:47 +1000

    Massively cleaned up using grid layout

--- src/components/widgets/WeatherWidget.vue
+++ src/components/widgets/WeatherWidget.vue
@@ -1,30 +1,26 @@
 <template>
     <div>
         <div class="bg-primary rounded-md flex p-2 flex-wrap text-themeText flex-col lg:flex-row" :class="{mt: !widgInfo['edit']}">
-            <div class="shadow rounded-md flex items-center flex-col
-                        border-solid border-gray-500 m-1 w-full lg:w-[calc(33%-8px)]
-                        justify-between"
+            <div class="day-item"
                  v-for="(day, i) in weather" :key="i">
-                <span class="font-semibold">
+                <span class="day-title">
                     {{day.time}}
                 </span>
-                <div class="flex min-w-[80%] max-w-[95%] justify-center">
-                    <div class="flex flex-col items-center mr-4 max-w-[60%]">
-                        <span class="material-symbols-outlined text-5xl text-blue-400">{{day['weathercode']?.icon}}</span>
-                        <span class="text-xs text-center px-1" :class="{fontL: widgInfo['col'] === 'leftCol'}">
-                            {{day['weathercode']?.message || "Loading..."}}
-                        </span>
+                <div class="day-weather">
+                    <span class="material-symbols-outlined text-5xl text-blue-400">{{day['weathercode']?.icon}}</span>
+                    <span class="text-xs text-center px-1" :class="{fontL: widgInfo['col'] === 'leftCol'}">
+                        {{day['weathercode']?.message || "Loading..."}}
+                    </span>
+                </div>
+                <div class="day-temp">
+                    <div class="dui-tooltip" data-tip="Minimum Temperature">
+                        <span class="text-xl text-blue-500">{{day["temperature_2m_min"]}}°</span>
                     </div>
-                    <div class="flex flex-col justify-center">
-                        <div class="dui-tooltip" data-tip="Minimum Temperature">
-                            <span class="text-xl text-blue-500">{{day["temperature_2m_min"]}}°</span>
-                        </div>
-                        <div class="dui-tooltip" data-tip="Maximum Temperature">
-                            <span class="text-xl text-red-600">{{day["temperature_2m_max"]}}°</span>
-                        </div>
+                    <div class="dui-tooltip" data-tip="Maximum Temperature">
+                        <span class="text-xl text-red-600">{{day["temperature_2m_max"]}}°</span>
                     </div>
                 </div>
-                <div class="flex mb-1">
+                <div class="day-details">
                     <div class="dui-tooltip" data-tip="Rain Chance">
                         <span class="material-symbols-outlined text-xl mr-1">rainy</span>
                         <span class="text-xl mr-2">{{day["precipitation_probability_mean"]}}%</span>
@@ -52,5 +48,33 @@ defineProps({
 <style scoped>
 .fontL {
     @apply text-sm
+}
+.day-item {
+    @apply shadow rounded-md border-solid border-gray-500 m-1 w-full p-1 lg:w-[calc(33%-8px)] grid;
+    grid-template-columns: 65% 30%;
+    grid-template-rows: 15% 70% 15%;
+}
+.day-title {
+    @apply font-semibold w-full text-center;
+    grid-column: 1 / span 2;
+    grid-row: 1;
+}
+.day-weather {
+    @apply flex flex-col items-center;
+    grid-column: 1;
+    grid-row: 2;
+}
+.day-temp {
+    @apply flex flex-col justify-center;
+    grid-column: 2;
+    grid-row: 2;
+}
+.day-details {
+    @apply flex mb-1 justify-center;
+    grid-column: 1 / span 2;
+    grid-row: 3;
+}
+.dui-tooltip {
+    @apply flex items-center;
 }
 </style>
