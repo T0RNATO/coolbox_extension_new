4e118cade33252ab014675b992e6f82fd3deaa6e
T0RNATO <nlittle315@gmail.com>
2023-08-29 15:14:58 +1000

    Added animated custom colour gradients to tiles

--- src/components/widgets/Tiles.vue
+++ src/components/widgets/Tiles.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul id="tileList" class="tileList">
+        <ul id="tileList" class="tileList" :style="animation_styles">
             <li v-for="(tile, index) in tiles"
                 :class="tile.className"
                 :style="getFilter(index)"
@@ -36,10 +36,10 @@
             </div>
             <input type="color" v-model="ts_hex1" v-if="ts_type === 'solid' || ts_type === 'gradient'">
             <input type="color" v-model="ts_hex2" v-if="ts_type === 'gradient'">
-
-<!--                <div v-if="ts_type === 'legacy_anim'">-->
-<!--                    RGB Speed:<input type="range" min="1" max="1400" class="dui-range" v-model="speed">-->
-<!--                </div>-->
+            <div v-if="ts_type === 'gradient'" class="flex items-center">
+                <input type="checkbox" id="anim" class="inline-block dui-checkbox" v-model="ts_animate">
+                <label for="anim">Animate gradient</label>
+            </div>
         </EditingContextMenu>
     </div>
 </template>
@@ -49,6 +49,7 @@ import EditingContextMenu from "~/components/EditingContextMenu.vue";
 import {useExtensionStorage} from "~/utils/componentUtils";
 import {statusMessages} from "~/utils/apiUtils";
 import {hexGradient} from "~/utils/utilFunctions";
+import {ref} from "vue";
 
 let tiles = document.querySelector('#tileList-2248').getElementsByClassName('tile');
 
@@ -56,7 +57,9 @@ const ts_type = useExtensionStorage("tiles.type", "legacy");
 const ts_hex1 = useExtensionStorage("tiles.hex1", "#3ae8d4");
 const ts_hex2 = useExtensionStorage("tiles.hex2", "#10739a");
 
-const speed = useExtensionStorage("tiles.speed", 1000);
+const ts_animate = useExtensionStorage("tiles.animate", false);
+
+const animation_styles = ref({});
 
 // Readable code stolen from https://stackoverflow.com/a/54070620/13102310
 function hexToHSV(hex) {
@@ -68,8 +71,14 @@ function hexToHSV(hex) {
     return [60*(h<0?h+6:h), v&&c/v, v];
 }
 
+function HSVToFilter(hsv) {
+    return `hue-rotate(${hsv[0] + 180}deg) saturate(${hsv[1] * 300}%) brightness(${hsv[2] * 1.5})`
+}
+
 function getFilter(i) {
-    const distanceFromBottomRight = (i%5+Math.floor(i/5)) * -1 + 15;
+    const distanceFromTopLeft = i%5+Math.floor(i/5)
+    const distanceFromBottomRight = distanceFromTopLeft * -1 + 7;
+
     let hsv = hexToHSV(ts_hex1.value);
 
     switch (ts_type.value) {
@@ -78,11 +87,16 @@ function getFilter(i) {
         case "legacy_anim":
             return {animation: `rgb 4s linear reverse infinite -${distanceFromBottomRight*150}ms`}
         case "gradient":
-            const gradientSteps = hexGradient(ts_hex1.value, ts_hex2.value, 7)
-            hsv = hexToHSV(gradientSteps[distanceFromBottomRight - 9]);
+            if (ts_animate.value) {
+                animation_styles.value["--start"] = HSVToFilter(hexToHSV(ts_hex1.value));
+                animation_styles.value["--end"] = HSVToFilter(hexToHSV(ts_hex2.value));
+                return {animation: `custom_animation 4s linear alternate-reverse infinite -${distanceFromBottomRight*300}ms`}
+            }
+            const gradientSteps = hexGradient(ts_hex1.value, ts_hex2.value, 7);
+            hsv = hexToHSV(gradientSteps[distanceFromTopLeft]);
         // intentional fallthrough
         case "solid":
-            return {filter: `hue-rotate(${hsv[0] + 180}deg) saturate(${hsv[1] * 300}%) brightness(${hsv[2] * 1.5})`};
+            return {filter: HSVToFilter(hsv)};
     }
     return {}
 }
@@ -122,6 +136,14 @@ li.tile:hover {
     }
     to {
         filter: hue-rotate(360deg) brightness(1.5) contrast(1.3);
+    }
+}
+@keyframes custom_animation {
+    from {
+        filter: var(--start)
+    }
+    to {
+        filter: var(--end)
     }
 }
 </style>
