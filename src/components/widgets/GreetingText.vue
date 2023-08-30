b5cd10d1ca597217639fc7a169152a55dfcde8fb
T0RNATO <nlittle315@gmail.com>
2023-08-30 18:58:19 +1000

    Added ability to change name in greeting text

--- src/components/widgets/GreetingText.vue
+++ src/components/widgets/GreetingText.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-      <h1 v-html="greeting"></h1>
-      <EditingContextMenu @delete="$emit('delete')"/>
-  </div>
+    <div>
+        <h1>
+            {{greeting.firstChild.textContent}}
+            <strong>{{customName || greeting.firstElementChild.textContent}}</strong>
+        </h1>
+        <EditingContextMenu @delete="$emit('delete')" settings="true">
+        <div class="option">
+            <label for="customName">Custom Name: </label>
+            <input type="text" id="customName" v-model="customName" class="ml-2">
+        </div>
+        </EditingContextMenu>
+    </div>
 </template>
 
 <script setup>
 import EditingContextMenu from "~/components/EditingContextMenu.vue";
+import {useExtensionStorage} from "~/utils/componentUtils";
 
-const greeting = document.querySelector('#component9 > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)').innerHTML;
+const customName = useExtensionStorage('customName', '');
+
+const greeting = document.querySelector('#component9 > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)');
 </script>
