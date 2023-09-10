47099b74625f698d76126068abfcfd65c41bc786
T0RNATO <nlittle315@gmail.com>
2023-09-10 22:36:29 +1000

    Cleaned up file

--- src/entries/contentScript/renderContent.js
+++ src/entries/contentScript/renderContent.js
@@ -1,16 +1,10 @@
 import browser from "webextension-polyfill";
 
-export default async function renderContent(
-    cssPaths,
-    render = (_appRoot) => {}
-) {
+export default async function renderContent(cssPaths, render = (_appRoot) => {}) {
     const appRoot = document.createElement("div");
 
     if (import.meta.hot) {
-        const { addViteStyleTarget } = await import(
-            "@samrum/vite-plugin-web-extension/client"
-            );
-
+        const { addViteStyleTarget } = await import("@samrum/vite-plugin-web-extension/client");
         await addViteStyleTarget(document.head);
     } else {
         cssPaths.forEach((cssPath) => {
@@ -21,8 +15,6 @@ export default async function renderContent(
         });
     }
 
-    document.querySelector("#content").style.display = "none"
     document.querySelector("#container").appendChild(appRoot);
-
     render(appRoot);
 }
