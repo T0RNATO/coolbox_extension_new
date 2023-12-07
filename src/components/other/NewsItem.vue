4b794579bfac1e267365af6a01a1b1aa13909305
T0RNATO <nlittle315@gmail.com>
2023-12-07 16:49:44 +1100

    Reimplemented news item saving

--- src/components/other/NewsItem.vue
+++ src/components/other/NewsItem.vue
@@ -1,5 +1,45 @@
+<template>
+    <div class="text-themeText p-2 relative" :class="{'bg-accent': item.sticky}">
+        <div class="flex">
+            <div class="w-full">
+                <a :href="item._links.self.href" :class="{bold: !item.viewedAt}">{{item.title}}</a>
+
+                <p class="blurb" v-html="item.blurb"></p>
+
+                <div class="flex justify-between">
+                    <span class="text-gray-500 italic text-sm">
+                        <img :src="item.author._links.avatar.href" :alt="item.author.fullname"
+                             class="rounded-full h-6 mr-2">
+                        <a :href="item.author._links.profile.href">{{item.author.fullname}}</a>
+                        {{item.publishAt.relativeTime}}
+                    </span>
+                    <div class="text-gray-500 italic text-sm" v-if="item.sticky">
+                        <span class="material-symbols-outlined">push_pin</span>
+                        Pinned
+                    </div>
+                </div>
+            </div>
+            <div v-if="item.featureImage">
+                <img :src="item.featureImage._links.image.href" alt="Feature Image"
+                    class="max-h-32 max-w-[160px]">
+            </div>
+        </div>
+        <hr class="m-0 mt-4">
+
+        <span class="absolute top-0 right-0 material-symbols-outlined pr-2 text-lg cursor-pointer bg-primary rounded-bl-md"
+              :class="{'icon-fill': saved}"
+              @click="save(item.id)"
+        >
+            star
+        </span>
+    </div>
+</template>
+
+<!--suppress TypeScriptUnresolvedReference (this is how to fix bugs as a good developer) -->
 <script setup lang="ts">
-defineProps<{
+import {cookieFetched} from "~/utils/apiUtils";
+
+const props = defineProps<{
     item: {
         id: number;
         title: string;
@@ -70,35 +110,34 @@ defineProps<{
         };
         attachments: number;
         viewedAt: null;
-    }
+    },
+    saved: Boolean
 }>();
-</script>
+const emit = defineEmits(["save", "unsave"]);
 
-<template>
-    <div class="text-themeText p-2" :class="{'bg-accent': item.sticky}">
-        <div class="flex">
-            <div>
-                <a :href="item._links.self.href" :class="{bold: !item.viewedAt}">{{item.title}}</a>
-                <p class="blurb">{{item.blurb}}</p>
-                <div class="flex justify-between">
-                    <span class="text-gray-500 italic text-sm">
-                        <img :src="item.author._links.avatar.href" :alt="item.author.fullname" class="rounded-full h-6 mr-2">
-                        <a :href="item.author._links.profile.href">{{item.author.fullname}}</a>
-                        {{item.publishAt.relativeTime}}
-                    </span>
-                    <div class="text-gray-500 italic text-sm" v-if="item.sticky">
-                        <span class="material-symbols-outlined">push_pin</span>
-                        Pinned
-                    </div>
-                </div>
-            </div>
-            <div v-if="item.featureImage" class="w-full">
-                <img :src="item.featureImage._links.image.href" alt="Feature Image">
-            </div>
-        </div>
-        <hr class="m-0 mt-4">
-    </div>
-</template>
+function post(url: string, cookie: string) {
+    return fetch("https://schoolbox.donvale.vic.edu.au/news/saved/" + url, {
+        "headers": {
+            "cookie": `PHPSESSID=${cookie}`
+        },
+        "method": "POST"
+    });
+}
+
+function save(id: number) {
+    cookieFetched.then(cookie => {
+        if (!props.saved) {
+            post(`${id}/add`, cookie).then(() => {
+                emit("save");
+            })
+        } else {
+            post(`${id}/remove`, cookie).then(() => {
+                emit("unsave");
+            })
+        }
+    })
+}
+</script>
 
 <style scoped>
 .blurb {
--- src/components/widgets/News.vue
+++ src/components/widgets/News.vue
@@ -1,9 +1,24 @@
 <template>
     <div>
-        <h2 class="subheader">News</h2>
-        <div class="overflow-y-auto h-96 rounded-lg bg-primary" v-if="newsItems.length">
-            <NewsItem :item="item" v-for="item in pinnedNewsItems"/>
-            <NewsItem :item="item" v-for="item in newsItems"/>
+        <h2 class="subheader inline">News</h2>
+        <span class="text-themeText">
+            -
+            <a href="/news/saved" class="dui-tooltip" data-tip="View Saved News Items">
+                <span class="material-symbols-outlined">
+                    hotel_class
+                </span>
+            </a>
+        </span>
+        <br>
+        <div class="overflow-y-auto h-[50vh] rounded-lg bg-primary" v-if="newsItems.length">
+            <NewsItem :item="item" v-for="item in pinnedNewsItems" :saved="savedItems.includes(item.id)"
+                      @save="savedItems.push(item.id)"
+                      @unsave="savedItems.splice(savedItems.indexOf(item.id), 1)"
+            />
+            <NewsItem :item="item" v-for="item in newsItems" :saved="savedItems.includes(item.id)"
+                      @save="savedItems.push(item.id)"
+                      @unsave="savedItems.splice(savedItems.indexOf(item.id), 1)"
+            />
         </div>
         <div class="dui-loading dui-loading-spinner" v-else/>
         <EditingContextMenu @delete="$emit('delete')"/>
@@ -19,7 +34,7 @@ import NewsItem from "~/components/other/NewsItem.vue";
 const pinnedNewsItems = ref([]);
 const newsItems = ref([]);
 
-// https://schoolbox.donvale.vic.edu.au/news/saved as well
+const savedItems = ref([]);
 
 cookieFetched.then(cookie => {
     fetch("https://schoolbox.donvale.vic.edu.au/news/lists/feed", {
@@ -35,6 +50,18 @@ cookieFetched.then(cookie => {
                 newsItems.value.push(item);
             }
         }
+    }))
+
+    fetch("https://schoolbox.donvale.vic.edu.au/news/saved", {
+        "headers": {
+            "cookie": `PHPSESSID=${cookie}`,
+
+            // Idk why this is needed but it returns html when not included
+            "x-requested-with": "XMLHttpRequest",
+        },
+        "method": "GET"
+    }).then(data => data.json().then(json => {
+        savedItems.value = json;
     }))
 })
 
