b901f2f7be84529929ed8f47be55d959310af075
T0RNATO <nlittle315@gmail.com>
2024-03-27 22:45:33 +1100

    Fixed not updating settings, updated to use TS

--- src/components/widgets/Timetable/Subject.vue
+++ src/components/widgets/Timetable/Subject.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import {roomChanges} from "~/utils/apiUtils";
 import {useExtensionStorage} from "~/utils/componentUtils";
-import {reactive} from "vue";
 
 const darkenSubjects = useExtensionStorage("timetable.dark", false);
 const outlineCurrent = useExtensionStorage("timetable.outline", false);
 
+type PrettySub = {
+    name: string,
+    pretty: string
+}
+
 const props = defineProps<{
     subject: HTMLElement,
-    pretty: Array<{
-        name: string,
-        pretty: string
-    }>,
-    period: number
+    pretty: PrettySub[],
+    period: number,
+    small_text: boolean
 }>();
 
 const roomChange = roomChanges.value?.find(
@@ -20,21 +22,23 @@ const roomChange = roomChanges.value?.find(
               change.period === props.period
 );
 
-const classes = reactive({
-    'darken': darkenSubjects,
-    'outline': outlineCurrent && props.subject.parentElement.classList.contains('timetable-subject-active'),
-    'cb-subject': true,
-});
+const subjectID = props.subject.children[1]?.textContent.slice(1,-1)?.toLowerCase();
 </script>
 
 <template>
-    <div :style="{backgroundColor: subject['style'].getPropertyValue('background-color')}" :class="classes">
+    <div :style="{backgroundColor: subject['style'].getPropertyValue('background-color')}"
+         :class="{
+            darken: darkenSubjects,
+            small: small_text,
+            outline: outlineCurrent && subject.parentElement.classList.contains('timetable-subject-active')
+        }"
+         class="cb-subject">
         <div v-if="subject.tagName === 'DIV'">
             <!--The title of the subject-->
-            <a :href="subject.firstElementChild?.href" class="cb-link">
+            <a :href="(subject.firstElementChild as HTMLAnchorElement)?.href" class="cb-link">
                 {{
                     pretty?.find(
-                        s => s['name']?.toLowerCase() === subject.children[1]?.textContent.slice(1,-1)?.toLowerCase()
+                        s => s['name']?.toLowerCase() === subjectID
                     )?.pretty || subject.firstElementChild?.textContent
                 }}
             </a>
@@ -55,3 +59,40 @@ const classes = reactive({
         </div>
     </div>
 </template>
+
+<style scoped>
+.cb-subject a {
+    color: #085ba5;
+}
+.cb-subject {
+    @apply p-4 lg:w-1/5 lg:h-auto text-sm sm:w-full sm:h-1/5 lg:first:rounded-bl-lg lg:last:rounded-br-lg sm:max-lg:first:rounded-tr-lg sm:last:rounded-br-lg;
+}
+.small {
+    font-size: 10px;
+    line-height: 12px;
+    @apply p-3;
+}
+.cb-subject.darken {
+    filter: brightness(0.8);
+}
+.cb-subject.outline {
+    outline: 2px solid #355983;
+    outline-offset: -2px;
+    position: relative;
+}
+.cb-subject.outline::after {
+    border-top: 10px solid #355983;
+    border-right: 10px solid #355983;
+    border-bottom: 10px solid transparent;
+    border-left: 10px solid transparent;
+    height: 20px;
+    aspect-ratio: 1/1;
+    position: absolute;
+    top: 0;
+    right: 0;
+    content: "";
+}
+.strike {
+    @apply line-through text-red-500;
+}
+</style>
