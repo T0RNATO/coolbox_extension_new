22e81097bbe179d12aa349de11d5b946c689125f
T0RNATO <nlittle315@gmail.com>
2024-05-05 22:25:23 +1000

    Made reactive extension storage more robust and added typing.

--- src/utils/componentUtils.ts
+++ src/utils/componentUtils.ts
@@ -1,36 +1,44 @@
-import browser from "webextension-polyfill";
-import {computed, ref, WritableComputedRef} from "vue";
+import browser, {Storage} from "webextension-polyfill";
+import {computed, ref, UnwrapRef, WritableComputedRef} from "vue";
+// @ts-ignore, vite-specific code
 import tailwind from "tailwindcss/tailwind.css?inline";
+// @ts-ignore, vite-specific code
 import icons from "/src/assets/css/icons.css?inline";
+
+let internalStorageChangeCount: number = 0;
 
 /**
  * Returns a reactive reference to Chrome extension storage
  */
-export function useExtensionStorage<T>(path: string, defaultV: T): WritableComputedRef<T> {
-    // The value that's being stored
+export function useExtensionStorage<T>(path: string, defaultV: T): WritableComputedRef<UnwrapRef<T>> {
+    // A reactive reference to the actual piece of data
     const storage = ref(defaultV);
 
-    let internalStorageChange = false;
-    // Check for when the actual storage changes, and update the ref
+    // Get the value from storage initially
+    browser.storage.local.get().then(data => {
+        try {
+            storage.value = objValueFromStringPath(data, path);
+        } catch {
+            console.log("setting default value", path)
+            // @ts-ignore, idek what's going on here
+            storage.value = defaultV;
+            setStorageValue(path, defaultV);
+        }
+    })
+
+    // Check for when browser storage changes, and update the value in the ref
     browser.storage.local.onChanged.addListener((changes) => {
+        if (internalStorageChangeCount > 0) {return}
+        if (changes['subjects']) {return}
+        console.log("external change", changes, path);
+
         // Get the first section of the path
         const p = path.split(".");
         const change = changes[p[0]];
 
-        if (change && !internalStorageChange) {
-            storage.value = objValueFromStringPath(change.newValue, p.slice(1).join("."), defaultV);
-        } else if (internalStorageChange) {
-            internalStorageChange = false;
-        }
-    })
-    // Get the value from storage initially
-    browser.storage.local.get().then(data => {
-        try {
-            storage.value = objValueFromStringPath(data, path, defaultV);
-        } catch {
-            storage.value = defaultV;
-            setStorageValue(path, defaultV);
-        }
+        if (!change) {return}
+
+        storage.value = objValueFromStringPath(change.newValue, p.slice(1).join("."), defaultV);
     })
     // Return a computed property that updates storage and returns the value
     return computed({
@@ -39,13 +47,30 @@ export function useExtensionStorage<T>(path: string, defaultV: T): WritableCompu
         },
         set(value) {
             storage.value = value;
-            internalStorageChange = true;
             setStorageValue(path, value);
         }
     })
 }
 
-function setStorageValue(path, value) {
+// Change a value in browser storage without messing with reactive refs to that storage
+export function manualStorageSet(data: object) {
+    internalStorageChangeCount++;
+    return browser.storage.local.set(data).then(() => {
+        internalStorageChangeCount--;
+    })
+}
+
+export function listenForStorageChange(path: string, callback: (storage: Storage.StorageChange) => void) {
+    browser.storage.local.onChanged.addListener((changes) => {
+        if (internalStorageChangeCount > 0 || !changes[path]) {return}
+        callback(changes[path]);
+    })
+}
+
+// Given a period-seperated path and a value, set the value in browser storage
+function setStorageValue(path: string, value: any) {
+    console.log("started setting internal storage change")
+    internalStorageChangeCount++;
     browser.storage.local.get().then(data => {
         let out = data;
         const layers = path.split(".");
@@ -59,20 +84,27 @@ function setStorageValue(path, value) {
             }
         }
         out[layers[layers.length - 1]] = value;
-        browser.storage.local.set(data)
+        browser.storage.local.set(data).then(() => {
+            console.log("reset internal storage change")
+            internalStorageChangeCount--;
+        })
     })
 }
 
-function objValueFromStringPath(obj, path, optionalDefault) {
-    const p = path.split(".");
-    let out = obj;
-    for (const section of p) {
-        out = out[section];
+// Given an object and a period-seperated path, return the value at that path
+function objValueFromStringPath(obj: object, path: string, optionalDefault?: any): any {
+    const pathSegments = path.split(".");
+    let output = obj;
+    // loop through the path segments and get the value
+    for (const segment of pathSegments) {
+        // may error, but will be caught
+        output = output[segment];
     }
-    if (out === undefined && optionalDefault) {
-        out = optionalDefault;
+    if (output === undefined) {
+        if (optionalDefault) {return optionalDefault}
+        throw new Error("Value not found in object");
     }
-    return out;
+    return output;
 }
 
 const twStyleSheet = new CSSStyleSheet();
@@ -81,7 +113,7 @@ const otherStyling = new CSSStyleSheet();
 
 twStyleSheet.replaceSync(tailwind);
 iconsStyleSheet.replaceSync(icons);
-// language=CSS
+
 otherStyling.replaceSync(`
     .dui-radio, .dui-checkbox {
         border: 1px solid rgb(107 114 128);
