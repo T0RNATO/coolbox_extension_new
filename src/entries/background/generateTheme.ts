ada1f9e8db96b0d9766224037314b79af3d51e54
T0RNATO <nlittle315@gmail.com>
2024-07-09 12:20:39 +1000

    refactor: Separate very large function into own file

---
+++ src/entries/background/generateTheme.ts
@@ -0,0 +1,67 @@
+import {hexToRGBString, shadeHexColor} from "../../utils/utilFunctions.ts";
+import {themePresets, legacyThemePresets} from "../../utils/themePresets.ts";
+import {AdvancedData, Theme} from "../../utils/types.ts";
+
+// Returns [cssString | null, shouldApplyBaseStyles]
+export function generateThemeCss(themeObject: Theme, font: string): [string | null, boolean] {
+    let variables: Partial<AdvancedData>;
+    switch (themeObject?.type || "preset") {
+        case "preset":
+            variables = themePresets.concat(legacyThemePresets).find(theme => theme.value === themeObject.presetData.preset)?.vars;
+            console.log(variables);
+            break;
+        case "legacy": {
+            const colour = themeObject.legacyData.colour;
+            if (themeObject.legacyData.style === "dark") {
+                variables = {
+                    "theme-text": "232 232 232",
+                    "theme-accent": hexToRGBString(colour),
+                    "theme-generic": hexToRGBString(shadeHexColor(colour, -0.2)),
+                    "link-colour": "89 169 255",
+                    "body-background": shadeHexColor(colour, -0.4),
+                    "navigation-background": colour,
+                }
+            } else {
+                variables = {
+                    "theme-text": "0 0 0",
+                    "theme-accent": hexToRGBString(shadeHexColor(colour, 0.4)),
+                    "theme-generic": hexToRGBString(shadeHexColor(colour, 0.2)),
+                    "link-colour": "52 94 231",
+                    "body-background": colour,
+                    "navigation-background": colour,
+                }
+            }
+            break;
+        }
+        case "custom":
+            const vars = themeObject.advancedData;
+            variables = {
+                "theme-text": hexToRGBString(vars["theme-text"]),
+                "theme-accent": hexToRGBString(vars["theme-accent"]),
+                "theme-generic": hexToRGBString(vars["theme-generic"]),
+                "link-colour": hexToRGBString(vars["link-colour"]),
+                "body-background": vars["body-background"],
+                "navigation-background": vars["navigation-background"],
+            }
+    }
+
+    if (!variables) {
+        if (font !== 'default') {
+            return [`*{font-family:"${font}"}`, false];
+        } else {
+            return [null, false];
+        }
+    }
+
+    let currentThemeCss = ":root {"
+    for (const [name, value] of Object.entries(variables)) {
+        if (name === "navigation-background" && !themeObject.changeNavbar) {
+            continue;
+        }
+        currentThemeCss += `--${name}: ${value} !important;`;
+    }
+    if (font !== 'default') {
+        currentThemeCss += `}*{font-family:"${font}"`
+    }
+    return [currentThemeCss + "}", true];
+}
--- src/entries/background/themes.ts
+++ src/entries/background/themes.ts
@@ -1,7 +1,6 @@
 import browser from "webextension-polyfill";
-import {hexToRGBString, shadeHexColor} from "../../utils/utilFunctions.ts";
-import {themePresets, legacyThemePresets} from "../../utils/themePresets.ts";
-import {AdvancedData, Theme} from "../../utils/types.ts";
+import {Theme} from "../../utils/types.ts";
+import {generateThemeCss} from "./generateTheme.ts";
 
 let customFont: string;
 
@@ -21,7 +20,7 @@ browser.runtime.onMessage.addListener((message: {newFont: string, oldFont: strin
 
 export function applyTheme(tab: number) {
     browser.storage.local.get("theme").then(result => {
-        const css = generateThemeCss(result.theme);
+        const css = generateThemeCss(result.theme, customFont);
         if (css[0]) {
             applyCss(tab, ...css);
         }
@@ -35,7 +34,7 @@ export function updateTheme(tab: number, oldT: Theme, newT: Theme, overrideOldFo
         files: ["/css/theme_base.css"]
     }).then(() => {
         const oldCss = generateThemeCss(oldT, overrideOldFont || customFont);
-        const newCss = generateThemeCss(newT);
+        const newCss = generateThemeCss(newT, customFont);
         if (oldCss[0]) {
             browser.scripting.removeCSS({
                 target: {tabId: tab},
@@ -61,69 +60,4 @@ function applyCss(tab: number, css: string, applyBase = true) {
         target: { tabId: tab },
         files: ["/css/theme_base.css"]
     }).then(applyTheme);
-}
-
-// Returns [cssString | null, shouldApplyBaseStyles]
-function generateThemeCss(themeObject: Theme, font = customFont): [string | null, boolean] {
-    console.log(themePresets, themeObject);
-    let variables: Partial<AdvancedData>;
-    switch (themeObject?.type || "preset") {
-        case "preset":
-            variables = themePresets.concat(legacyThemePresets).find(theme => theme.value === themeObject.presetData.preset)?.vars;
-            console.log(variables);
-            break;
-        case "legacy": {
-            const colour = themeObject.legacyData.colour;
-            if (themeObject.legacyData.style === "dark") {
-                variables = {
-                    "theme-text": "232 232 232",
-                    "theme-accent": hexToRGBString(colour),
-                    "theme-generic": hexToRGBString(shadeHexColor(colour, -0.2)),
-                    "link-colour": "89 169 255",
-                    "body-background": shadeHexColor(colour, -0.4),
-                    "navigation-background": colour,
-                }
-            } else {
-                variables = {
-                    "theme-text": "0 0 0",
-                    "theme-accent": hexToRGBString(shadeHexColor(colour, 0.4)),
-                    "theme-generic": hexToRGBString(shadeHexColor(colour, 0.2)),
-                    "link-colour": "52 94 231",
-                    "body-background": colour,
-                    "navigation-background": colour,
-                }
-            }
-            break;
-        }
-        case "custom":
-            const vars = themeObject.advancedData;
-            variables = {
-                "theme-text": hexToRGBString(vars["theme-text"]),
-                "theme-accent": hexToRGBString(vars["theme-accent"]),
-                "theme-generic": hexToRGBString(vars["theme-generic"]),
-                "link-colour": hexToRGBString(vars["link-colour"]),
-                "body-background": vars["body-background"],
-                "navigation-background": vars["navigation-background"],
-            }
-    }
-
-    if (!variables) {
-        if (font !== 'default') {
-            return [`*{font-family:"${font}"}`, false];
-        } else {
-            return [null, false];
-        }
-    }
-
-    let currentThemeCss = ":root {"
-    for (const [name, value] of Object.entries(variables)) {
-        if (name === "navigation-background" && !themeObject.changeNavbar) {
-            continue;
-        }
-        currentThemeCss += `--${name}: ${value} !important;`;
-    }
-    if (font !== 'default') {
-        currentThemeCss += `}*{font-family:"${font}"`
-    }
-    return [currentThemeCss + "}", true];
 }
