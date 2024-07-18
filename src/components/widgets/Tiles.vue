1587d2de34fef8fdd57ebe8bd2b9cf198fbdb72a
T0RNATO <nlittle315@gmail.com>
2024-07-18 12:53:16 +1000

    fix: Replace old filter-based system with new background-removing system to allow for tiles with different initial colours

--- src/components/widgets/Tiles.vue
+++ src/components/widgets/Tiles.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
-        <ul id="tileList" class="tileList" :style="animation_styles">
-            <li v-for="(tile, index) in tiles"
-                :class="tile.className"
-                :style="getFilter(index)"
-                :id="tile.id"
-                v-html="tile.innerHTML"
-                @dragstart.prevent
-            >
-            </li>
-        </ul>
+        <!--I have very little idea how this works but it removes all colours except those where r,g,b are equal-->
+        <svg width="0" height="0">
+            <filter id="keep-white" color-interpolation-filters="sRGB" width="100%" height="100%" x="0%" y="0%">
+                <feColorMatrix type="matrix" values="
+                  1 0 0 0 0
+                  0 1 0 0 0
+                  0 0 1 0 0
+                  -1 -1 -1 3.0001 0" />
+                <feComponentTransfer>
+                    <feFuncA type="linear" slope="-10" intercept="10"/>
+                </feComponentTransfer>
+            </filter>
+        </svg>
+
+        <div id="tileList" class="tileList grid grid-cols-6 gap-2" :style="animation_styles">
+            <div class="cb-tile w-full aspect-square rounded-md"
+                 v-for="(tile, index) in tiles"
+                 :style="ts_type == 'default' ? {} : tileStyle(index)">
+                <a :href="tile.querySelector('a').href" target="_blank">
+                    <div class="!w-full !h-full tile bg-center bg-contain !m-0 !p-0"
+                        :style="ts_type == 'default' ? {} : {filter: 'url(#keep-white)'}"
+                        :id="tile.id"
+                        @dragstart.prevent
+                    />
+                </a>
+            </div>
+        </div>
 
         <span class="text-themeText whitespace-pre-line">{{statusMessages.info}}</span>
 
@@ -44,16 +61,20 @@
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import EditingContextMenu from "~/components/other/EditingContextMenu.vue";
 import {useExtensionStorage} from "~/utils/componentUtils";
 import {statusMessages} from "~/utils/apiUtils";
 import {hexGradient} from "~/utils/utilFunctions";
-import {ref} from "vue";
+import {ref, type WritableComputedRef} from "vue";
+import type {widgInfo} from "~/utils/types.ts";
 
-let tiles = document.querySelector('#tileList-2248').getElementsByClassName('tile');
+const tiles = document.querySelector('#tileList-2248').getElementsByClassName('tile');
+const numberOfTiles = tiles.length;
 
-const ts_type = useExtensionStorage("tiles.type", "legacy");
+type TileType = "legacy" | "solid" | "gradient" | "default" | "legacy_anim";
+
+const ts_type: WritableComputedRef<TileType> = useExtensionStorage("tiles.type", "legacy" as TileType);
 const ts_hex1 = useExtensionStorage("tiles.hex1", "#3ae8d4");
 const ts_hex2 = useExtensionStorage("tiles.hex2", "#10739a");
 
@@ -61,92 +82,69 @@ const ts_animate = useExtensionStorage("tiles.animate", false);
 
 const animation_styles = ref({});
 
-// Readable code stolen from https://stackoverflow.com/a/54070620/13102310
-function hexToHSV(hex) {
-    let r = parseInt(hex.substring(1,3),16)/255;
-    let g = parseInt(hex.substring(3,5),16)/255;
-    let b = parseInt(hex.substring(5,7),16)/255;
-    let v = Math.max(r,g,b), c=v-Math.min(r,g,b);
-    let h = c && ((v===r)?(g-b)/c:((v===g)?2+(b-r)/c:4+(r-g)/c));
-    return [60*(h<0?h+6:h), v&&c/v, v];
-}
-
-function HSVToFilter(hsv) {
-    return `hue-rotate(${hsv[0] + 180}deg) saturate(${hsv[1] * 300}%) brightness(${hsv[2] * 1.5})`
-}
-
-function getFilter(i) {
-    const distanceFromTopLeft = i%5+Math.floor(i/5)
+function tileStyle(i): {backgroundColor?: string, animation?: string} {
+    const distanceFromTopLeft = i%6+Math.floor(i/6);
     const distanceFromBottomRight = distanceFromTopLeft * -1 + 7;
 
-    let hsv = hexToHSV(ts_hex1.value);
-
     switch (ts_type.value) {
+        case "solid": {
+            return {backgroundColor: ts_hex1.value};
+        }
         case "legacy": {
-            return {filter: `hue-rotate(${getHueRotation(i)}deg) brightness(1.5) contrast(1.3)`};
+            if (i >= 6 && i < 12) {
+                i = 11 - (i - 6);
+            }
+            return {
+                backgroundColor: `hsl(${360 / numberOfTiles * i} 45% 55%)`
+            }
         }
         case "legacy_anim": {
-            return {animation: `rgb 4s linear reverse infinite -${distanceFromBottomRight * 150}ms`};
+            return {
+                animation: `rgb 4s linear reverse infinite -${distanceFromBottomRight * 150}ms`,
+                backgroundColor: `rgb(255, 0, 0)`
+            }
         }
         case "gradient": {
             if (ts_animate.value) {
-                animation_styles.value["--start"] = HSVToFilter(hexToHSV(ts_hex1.value));
-                animation_styles.value["--end"] = HSVToFilter(hexToHSV(ts_hex2.value));
-                return {animation: `custom_animation 3s linear alternate-reverse infinite -${distanceFromBottomRight*300}ms`}
+                animation_styles.value["--start"] = ts_hex1.value;
+                animation_styles.value["--end"] = ts_hex2.value;
+                return {
+                    animation: `custom_animation 3s linear alternate-reverse infinite -${distanceFromBottomRight*300}ms`,
+                }
             }
             const gradientSteps = hexGradient(ts_hex1.value, ts_hex2.value, 7);
-            hsv = hexToHSV(gradientSteps[distanceFromTopLeft]);
-        } // intentional fallthrough
-        case "solid": {
-            return {filter: HSVToFilter(hsv)};
+            return {
+                backgroundColor: gradientSteps[distanceFromTopLeft],
+            }
         }
     }
     return {}
 }
 
-function getHueRotation(index) {
-  if (index > 4 && index < 10) {
-      // reverse the order of the tiles for row 2
-      return (14 - index) * 24;
-  }
-  return index * 24;
-}
-
-defineProps({
-    widgInfo: Object
-})
+defineProps<{
+    widgInfo: widgInfo
+}>();
 </script>
 
 <style>
-#tileList.tileList .tile {
-    width: 17%;
-    margin: 1.5%;
-    padding-bottom: 17%;
-}
-li.tile {
-    border-radius: 5px;
-    width: 19% !important;
-    margin: 0.5% !important;
-    padding-bottom: 18% !important;
-}
-li.tile:hover {
+.cb-tile:hover {
     transform: scale(1.05);
     transition: all 0.05s ease-in-out;
 }
 @keyframes rgb {
     from {
-        filter: hue-rotate(0deg) brightness(1.5) contrast(1.3);
+        filter: hue-rotate(0deg);
     }
     to {
-        filter: hue-rotate(360deg) brightness(1.5) contrast(1.3);
+        filter: hue-rotate(360deg);
     }
 }
 @keyframes custom_animation {
     from {
-        filter: var(--start)
+        background-color: var(--start);
     }
     to {
-        filter: var(--end)
+        background-color: var(--end);
     }
 }
 </style>
