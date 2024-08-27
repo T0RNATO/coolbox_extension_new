158db83cf89dab7ab741fc8958f9044fc961fa0f
T0RNATO <nlittle315@gmail.com>
2024-08-27 11:11:47 +1000

    feat: Show two todo lists in widget when in left column

--- src/components/widgets/TodoListWidget.vue
+++ src/components/widgets/TodoListWidget.vue
@@ -7,12 +7,17 @@ import {ref, type Ref} from "vue";
 import TodoList from "~/entries/pages/todo/TodoList.vue"
 
 const todoLists: Ref<TodoListType[]> = ref([]);
-const loaded: Ref<boolean> = ref(false);
+// noinspection TypeScriptValidateTypes
+const state: Ref<"loaded" | "loading" | "no_lists"> = ref("loading");
 
 cookieFetched.then(() => {
     getCachedTodoLists().then((data: TodoListType[]) => {
         todoLists.value = data;
-        loaded.value = true;
+        if (data.length === 0) {
+            state.value = "no_lists";
+            return;
+        }
+        state.value = "loaded";
     })
 })
 
@@ -28,31 +33,44 @@ function inc() {
     displayedListIndex.value %= todoLists.value.length;
 }
 
-defineProps<{
+const props = defineProps<{
     widgInfo: widgInfo
 }>();
+
+console.log(props.widgInfo);
 </script>
 
 <template>
 <div>
-    <div v-if="loaded" class="flex flex-col items-stretch w-full todo-container bg-primary rounded-md">
-        <Transition mode="out-in">
-            <TodoList :list="todoLists[displayedListIndex]" :key="displayedListIndex" :widget="true"/>
+    <div v-if="state === 'loaded'" class="flex flex-col items-stretch w-full todo-container bg-primary rounded-md">
+        <Transition mode="out-in" v-if="widgInfo.col == 'rightCol'">
+            <TodoList :list="todoLists[displayedListIndex]" :widget="true" :key="displayedListIndex"/>
+        </Transition>
+        <Transition mode="out-in" v-else>
+            <div :key="displayedListIndex" class="flex">
+                <TodoList :list="todoLists[displayedListIndex]" :widget="true" style="width: 100%;"/>
+                <TodoList v-if="todoLists.length > 1" :list="todoLists[(displayedListIndex + 1) % todoLists.length]" :widget="true" style="width: 100%;"/>
+            </div>
         </Transition>
         <div class="w-full flex justify-center pb-2 bg-primary rounded-b-md">
             <span class="cb-icon text-2xl" @click="dec">chevron_left</span>
             <span class="cb-icon text-md"
                   v-for="(_, i) in todoLists"
-                  :class="{'icon-fill': i == displayedListIndex}"
+                  :class="{'icon-fill':
+                      i == displayedListIndex || (widgInfo.col == 'leftCol' && i == (displayedListIndex + 1) % todoLists.length)
+                  }"
                   @click="displayedListIndex = i"
             >circle</span>
             <span class="cb-icon text-2xl" @click="inc">chevron_right</span>
         </div>
     </div>
-    <div v-else class="bg-primary p-2 rounded-md">
+    <div v-else-if="state === 'loading'" class="bg-primary p-2 rounded-md">
         <span class="text-themeText">Loading Todo List..</span>
         <br/>
         <div class="dui-loading dui-loading-spinner"/>
+    </div>
+    <div v-else-if="state === 'no_lists'" class="bg-primary p-2 rounded-md text-themeText">
+        You have no to-do lists. Create one <a href="/coolbox-todo">on the to-do page</a>.
     </div>
     <EditingContextMenu @delete="$emit('delete')"/>
 </div>
