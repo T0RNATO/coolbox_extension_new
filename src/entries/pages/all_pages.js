f35cb4a8499ec21b675d2b1be6da4dda7361c335
SleepyStew <sleepystew3@gmail.com>
2025-11-01 22:22:26 +1100

    Revert "feat: legacy day #2"
    This reverts commit e9200c0edfe85e1aca9ce17e496c585b70f623cc.

--- src/entries/pages/all_pages.js
+++ src/entries/pages/all_pages.js
@@ -7,7 +7,6 @@ import browser from "webextension-polyfill";
 import {apiSend, cookieFetched} from "~/utils/apiUtils";
 import {addViteStyleTarget} from "@samrum/vite-plugin-web-extension/client";
 import TodoPage from "~/entries/pages/todo/TodoPage.vue";
-import AnnoyingCookiePrompt from "~/components/other/AnnoyingCookiePrompt.vue";
 
 if (import.meta.env.DEV) {
     import("tailwindcss/tailwind.css");
@@ -22,14 +21,6 @@ const VueInjections = {
 }
 
 const appProps = reactive({subjects: [{name: 'Loading...', pretty: 'Loading...'}]})
-
-// Mount the prank overlay and react to settings
-const overlayRoot = document.createElement("div");
-document.body.appendChild(overlayRoot);
-const overlayApp = createApp({
-    render: () => h(AnnoyingCookiePrompt)
-});
-overlayApp.mount(overlayRoot);
 
 // Render Vue
 if (location.pathname in VueInjections) {
